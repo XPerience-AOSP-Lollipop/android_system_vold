23431418eb36c5a3021c617414fd16b4665fc7a0
myfluxi <linflux@arcor.de>
2016-01-13 22:36:18 +0100

    cryptfs: Set HW FDE flags when building libvold
    Change-Id: I62e8fb77e60944edfb2a626c99df516cf959c4cc

--- Android.mk
+++ Android.mk
@@ -130,6 +130,12 @@ LOCAL_CFLAGS := $(vold_cflags)
 LOCAL_CONLYFLAGS := $(vold_conlyflags)
 LOCAL_REQUIRED_MODULES := $(required_modules)
 
+ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
+TARGET_CRYPTFS_HW_PATH ?= device/qcom/common/cryptfs_hw
+LOCAL_C_INCLUDES += $(TARGET_CRYPTFS_HW_PATH)
+LOCAL_CFLAGS += -DCONFIG_HW_DISK_ENCRYPTION
+endif
+
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -153,6 +159,10 @@ LOCAL_CONLYFLAGS := $(vold_conlyflags)
 LOCAL_SHARED_LIBRARIES := $(common_shared_libraries)
 LOCAL_STATIC_LIBRARIES := $(common_static_libraries)
 LOCAL_REQUIRED_MODULES := $(required_modules)
+
+ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
+LOCAL_SHARED_LIBRARIES += libcryptfs_hw
+endif
 
 include $(BUILD_EXECUTABLE)
 
